fb4984eea9bcab98f169afe7689d34963fefc8d9
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-06-25 15:27:50 +0200

    fixed test

--- cypress/integration/about-page.js
+++ cypress/integration/about-page.js
@@ -2,8 +2,3 @@ it("About page", () => {
   cy.log(`Visiting /about/`);
   cy.visit("/about/");
 });
-
-it("Non-existing page", () => {
-  cy.log(`Visiting /aboutaaa/`);
-  cy.visit("/aboutaaa/");
-});
